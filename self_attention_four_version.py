deb00b9b9565500385774463f444d3330644d4da
张乐骏 <zhangyuejun@zhangyuejundeMacBook-Air.local>
2025-06-17 00:18:18 -0400

    3versions

--- self_attention_four_version.py
+++ self_attention_four_version.py
@@ -3,8 +3,8 @@ import torch
 import torch.nn as nn
 print(torch.__version__)
 
-## version 1
-class SelfAttention(nn.Module):
+## version 1 : normal version
+class SelfAttentionv1(nn.Module):
     def __init__(self,embedding_dim: int  = 768) -> None:
         super().__init__()
         self.embedding_dim = embedding_dim
@@ -29,3 +29,65 @@ class SelfAttention(nn.Module):
         output = torch.matmul(attention_weight , v)
 
         return output
+    
+## version 2 : optimize matrix calculations
+class SelfAttentionV2(nn.Moudle):
+    def __init__(self,embedding):
+        super().__init__()
+        self.embedding = embedding
+        self.proj = nn.Linear(embedding,embedding*3)
+
+    def forward(self,x):
+        QKV = self.proj(x)
+        Q , K , V = torch.split(QKV,self.embedding,dim = -1)
+        attention_weight = torch.softmax(
+            torch.matmul(Q,K.transpose(-1,-2)) / math.sqrt(self.embedding),
+            dim = -1
+        )
+        print(attention_weight)
+        output = torch.matmul(attention_weight,V)
+
+        return output
+    
+
+## version 3 : add some details 
+## 1. dropout position
+## 2. attetion mask
+## 3. output matrix projection
+
+class SelfAttentionV3(nn.Module):
+    def __init__(self,embedding,dropout_rate = 0.1):
+        super().__init__()
+        self.embedding = embedding
+        self.proj = nn.Linear(embedding,embedding*3)
+        self.attention_dropout = nn.Dropout(dropout_rate)
+        self.output_proj = nn.Linear(embedding,embedding)
+
+    def forward(self,x,attention_mask = None):
+        # x(batch, seq, embedding)
+        QKV = self.proj(x)
+        Q, K, V = torch.split(QKV, self.embedding, dim = -1)
+
+        #attention_weight(batch, seq, seq)
+        attention_weight = Q @ K.transpose(-1, -2) / math.sqrt(self.embedding)
+        if attention_mask is not None:
+            attention_weight = attention_weight.masked_fill(
+                attention_mask == 0,
+                float("-1e20")
+            )
+        attention_weight = torch.softmax(
+            attention_weight,
+            dim = -1
+        )
+        attention_result = attention_weight @ V
+        output = self.output_proj(attention_result)
+        return output
+ 
+
+ 
+
+    
+
+
+
+
